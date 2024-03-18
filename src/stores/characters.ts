3baaa0c1671067dc8fd1c8f0704b248f6e255d00
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-17 21:53:05 -0300

    ADD: Search character function.

--- src/stores/characters.ts
+++ src/stores/characters.ts
@@ -1,16 +1,18 @@
 import { defineStore } from "pinia";
+const apiURL = "https://rickandmortyapi.com/api/character"
 
 export const useCharactersStore = defineStore("characters",{
   state: () => {
     return {
       characters: [] as Character[],
       singleCharacter: {} as Character,
+      searchResults: [] as Character[],
     }
   },
   actions: {
     async getCharacters() {
       try {
-        const res = await fetch("https://rickandmortyapi.com/api/character")
+        const res = await fetch(apiURL)
         const data = await res.json()
         this.characters = data.results
       } catch (error) {
@@ -19,12 +21,21 @@ export const useCharactersStore = defineStore("characters",{
     },
     async getSingleCharacter(id: number) {
       try {
-        const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        const res = await fetch(`${apiURL}/${id}`)
         const data = await res.json()
         this.singleCharacter = data
       } catch (error) {
         console.error(error)
       }
     },
+    async searchCharacter(name: string) {
+      try {
+        const res = await fetch(`${apiURL}/?name=${name}`)
+        const data = await res.json()
+        this.searchResults = data.results
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
 })
